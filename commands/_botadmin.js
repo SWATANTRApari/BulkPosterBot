1c00cb98e49613b8b0c7604871ca502763d8c771
SWATANTRApari <102210380+SWATANTRApari@users.noreply.github.com>
2023-05-22 02:17:45 +0530

    Update _botadmin.js

--- commands/_botadmin.js
+++ commands/_botadmin.js
@@ -19,6 +19,6 @@ CMD*/
 
 Api.getChatMember({
 chat_id: ""+params+"",
-user_id: 6221461075, //Message @BotsChats to get your bot Telegram id
+user_id: 6079059764, //Message @BotsChats to get your bot Telegram id
 on_result: "/check "+params+""
 })
